0a55a7285908e7c62622b2f0c28982d309312a8a
Tim Greene <tim.g.greene@gmail.com>
2021-11-08 09:16:06 +0000

    Add em breakpoint handling
    I imagine there are improvements we could make here, but functioning okay for now

--- src/main/breaking_point/core.cljs
+++ src/main/breaking_point/core.cljs
@@ -1,8 +1,7 @@
 (ns breaking-point.core
   (:require
    [re-frame.core :as rf]
-   [re-frame-fx.dispatch]
-   ))
+   [re-frame-fx.dispatch]))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -18,13 +17,19 @@
    {::set-breakpoints opts}))
 
 
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Implementation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; COFX
+
+(defn get-font-size
+  []
+  (-> "body"
+      js/window.document.querySelector
+      js/window.getComputedStyle
+      (goog.object/get "font-size")
+      (js/parseFloat)))
 
 (rf/reg-cofx
  ::screen-dimensions
@@ -44,21 +49,27 @@
                                    .-clientHeight)
                            (some-> js/document
                                    .-body
-                                   .-clientHeight))]
+                                   .-clientHeight))
+         font-size (get-font-size)]
      (assoc coeffect
             :screen-width screen-width
-            :screen-height screen-height))))
-
+            :screen-height screen-height
+            :screen-width-em (/ screen-width font-size)
+            :screen-height-em (/ screen-height font-size)))))
 
 ;; Events
 
 (defn set-screen-dimensions
   [{:keys [db
            screen-width
-           screen-height]} _]
+           screen-height
+           screen-width-em
+           screen-height-em]} _]
   {:db (-> db
            (assoc-in [::breakpoints :screen-width] screen-width)
-           (assoc-in [::breakpoints :screen-height] screen-height))})
+           (assoc-in [::breakpoints :screen-height] screen-height)
+           (assoc-in [::breakpoints :screen-width-em] screen-width-em)
+           (assoc-in [::breakpoints :screen-height-em] screen-height-em))})
 
 (rf/reg-event-fx ::set-screen-dimensions
                  [(rf/inject-cofx ::screen-dimensions)]
@@ -80,16 +91,26 @@
 (defn get-screen-height [db _]
   (get-in db [::breakpoints :screen-height]))
 
+(defn get-screen-width-em [db _]
+  (get-in db [::breakpoints :screen-width-em]))
+
+(defn get-screen-height-em [db _]
+  (get-in db [::breakpoints :screen-height-em]))
+
 
 (defn ->get-screen [breakpoints]
   (fn get-screen
-    [screen-width _]
+    [[screen-width screen-width-em] _]
     (when screen-width
       (reduce
        (fn [prev-breakpoint [screen-key breakpoint]]
          (if (or (nil? breakpoint)
-                 (and (< screen-width breakpoint)
-                      (>= screen-width prev-breakpoint)))
+                 (and (if (and (string? breakpoint) (re-matches #"\d+em" breakpoint))
+                        (< screen-width-em (js/parseInt breakpoint))
+                        (< screen-width breakpoint))
+                      (if (and (string? breakpoint) (re-matches #"\d+em" prev-breakpoint))
+                        (>= screen-width-em (js/parseInt prev-breakpoint))
+                        (>= screen-width prev-breakpoint))))
            (reduced screen-key)
            breakpoint))
        0
@@ -108,9 +129,12 @@
 (defn register-subs [breakpoints]
   (rf/reg-sub ::screen-width get-screen-width)
   (rf/reg-sub ::screen-height get-screen-height)
+  (rf/reg-sub ::screen-width-em get-screen-width-em)
+  (rf/reg-sub ::screen-height-em get-screen-height-em)
 
   (rf/reg-sub ::screen
               :<- [::screen-width]
+              :<- [::screen-width-em]
               (->get-screen breakpoints))
 
   (rf/reg-sub ::orientation
